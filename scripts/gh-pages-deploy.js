679b0dce16a7bc0cd75782fc0f8634ba03e294cc
George Prichici <4246227+georgeprichici@users.noreply.github.com>
2020-05-02 16:32:10 -0700

    Initial Github pages deploy script.

---
+++ scripts/gh-pages-deploy.js
@@ -0,0 +1,19 @@
+require('shelljs/global');
+
+
+
+var main=function() {
+    mkdir('../.tmp');
+    cp('out/html/index.html ../.tmp/index.html');
+    
+    cd('../.tmp');
+    exec('git init');
+    exec('git config user.name "Travis-CI"');
+    exec('git config user.email "travis@travis"');
+    exec('git add .');
+    exec('git commit -m "Deployed to Github Pages"');
+    var repoUrl = "https://" + process.env.GITHUB_TOKEN + '@github.com/georgeprichici/metadefender-core-openapi3.git'
+    exec('git push --force "'+ repoUrl + '" master:gh-pages');
+}
+
+main();
